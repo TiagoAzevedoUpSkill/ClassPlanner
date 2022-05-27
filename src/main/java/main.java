b6346a46fd5e909d30c48aa83edfcbcc15b477b5
tiagoazeved0 <tiagomina.azevedo@upskill.pt>
2022-05-27 18:05:24 +0100

    Updates to pom.xml

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,6 +2,7 @@ import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
@@ -15,9 +16,9 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.ZoneId;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class main {
@@ -25,30 +26,63 @@ public class main {
         CommandLine commandLine = parseArguments(args);
         String username = commandLine.getOptionValue("username");
         String password = commandLine.getOptionValue("password");
-        String box = commandLine.getOptionValue("box");
-        String cookie = login(username, password, box);
-        getAllClassesByDay("regybox_user=" + cookie, LocalDate.of(2022,05,26));
+        String id_box = commandLine.getOptionValue("id_box");
+        String cookie = login(username, password, id_box);
+        getAllClassIds("regybox_user=" + cookie, LocalDate.of(2022,05,29));
 
+
+    }
+
+//    private static void scheduleClassByDayAndHour(String cookie, LocalDate date, String hour) {
+////        getClassId(cookie, date);
+////        isClassAvailable(cookie);
+////        getURLClassParams(getAllClassesByDay(cookie, date, "marca_aulas.php?"), hour);
+//
+//    }
+
+
+//    private static String getSingleClassId(String cookie, LocalDate date) {
+//        return "";
+//    }
+
+    private static void getAllClassIds(String cookie, LocalDate date) {
+        String teste = getAllClassesByDay(cookie, date);
+
+        Matcher m = Pattern.compile("valor3=.{4}").matcher(teste);
+        Map<String, String> parameters = new HashMap<>();
+        while (m.find())
+        {
+            parameters.put("valor3=",m.group());
+        }
+    }
+
+    private static String getDateEpochParam(LocalDate date) {
+        return Long.toString(date.atStartOfDay().atZone(ZoneId.of("Europe/Lisbon")).toInstant().toEpochMilli());
+    }
+
+
+//    private static boolean isClassAvailable(String cookie, LocalDate date, String hour, String id_box) {
+//
+//        // id_box (we got this) and class id (we dont have this yet) needed
+//        return false;
+//    }
+
+    private static String getURLClassParams(String allClassesByDay, String hour, String separator) {
+        String subStringURL = StringUtils.substringAfter(allClassesByDay.split(hour)[1], separator);
+        return subStringURL.substring(0, subStringURL.indexOf("'"));
     }
 
     private static String getAllClassesByDay(String cookie, LocalDate date) {
         String url = "https://www.regybox.pt/app/app_nova/php/aulas/aulas.php";
-        String dateParam = Long.toString(date.atStartOfDay().atZone(ZoneId.of("Europe/Lisbon")).toInstant().toEpochMilli());
         Map<String, String> parameters = new HashMap<>();
-        parameters.put("valor1", dateParam);
+        parameters.put("valor1", getDateEpochParam(date));
 
         String form = toURLParameters(parameters);
-
         HttpClient client = HttpClientBuilder.create().build();
-
         HttpGet getRequest = new HttpGet(url + "?" + form);
-
         getRequest.addHeader("Cookie", cookie);
 
-        final String response = getResponse(client, getRequest);
-
-        System.out.println(response);
-        return response;
+        return getResponse(client, getRequest);
     }
 
     private static String login(String username, String password, String id_box) {
@@ -60,9 +94,7 @@ public class main {
         String form = toURLParameters(parameters);
 
         HttpClient client = HttpClientBuilder.create().build();
-
         HttpPost post = new HttpPost(url);
-
         post.setEntity(new StringEntity(form, StandardCharsets.UTF_8));
         post.addHeader("Content-Type", "application/x-www-form-urlencoded");
 
@@ -73,7 +105,6 @@ public class main {
 
     private static String obterCookie(String response) {
         String array = response.split("\n")[1];
-
         return array.substring(array.indexOf("=") + 1, array.indexOf("&"));
     }
 
@@ -81,7 +112,6 @@ public class main {
         final String response;
         try {
             response = client.execute(requestBase, new BasicResponseHandler());
-            System.out.println("Response: " + response);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -97,7 +127,7 @@ public class main {
         final Options options = new Options();
         options.addOption("", "username", true, "");
         options.addOption("", "password", true, "");
-        options.addOption("", "box", true, "");
+        options.addOption("", "id_box", true, "");
 
         final CommandLine commandLine;
         try {
